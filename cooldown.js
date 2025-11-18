93e465dfbf8e1508475b4f0a1e021367f2eff1f1
Kjerne <59027271+Kjerne@users.noreply.github.com>
2025-11-18 05:15:05 +0100

    Update cooldown.js

--- cooldown.js
+++ cooldown.js
@@ -1,6 +1,3 @@
-/**
- * Returns a random cooldown in milliseconds between min and max
- */
 function getRandomCooldown(min, max) {
   if (min > max) [min, max] = [max, min];
   return Math.floor(Math.random() * (max - min + 1)) + min;
